5d0ac247c5951e0dbad0fac0c47b1ebcc4f709d6
zubaka <zubaka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-16 21:33:01 +0000

    Adding first version zbPlayer plugin
    git-svn-id: https://plugins.svn.wordpress.org/zbplayer/trunk@653891 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,47 @@
+===zbPlayer ===
+Contributors: zubaka
+Donate link: http://gilevich.com/
+Tags: mp3, flash player, audio, easy control, noJS, small mp3 player, embed, media player, music player, mp3 player
+Requires at least: 3.5
+Tested up to: 3.5
+Stable tag: 1.0
+License: Dual Licensed under the MIT and GPLv2 or later
+
+zbPlayer is a small and very easy plugin. It does one thing: capture mp3 links and insert a small flash player instead.
+
+== Description ==
+
+zbPlayer is a very easy audio plugin - you can select some options like: include Download link or no, enable autoplay or no and setup width of player. Other things will be done by zbPlayer plugun automatically.
+
+== Installation ==
+
+The most basic installation is a simple two step:
+
+1. Upload the `zbplayer` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+That's it, you're done.
+
+== Frequently Asked Questions ==
+
+= Do I need to do anything special in my posts? =
+
+No, zbPlayer automatically converts any link to an mp3 file into player. So, if you put in &lt;a href="audio_file.mp3"&gt;A Link&lt;/a&gt; zbPlayer will automatically add a flash player on page.
+
+== Screenshots ==
+
+1.  The minimized player.
+2.  The expanded player.
+
+
+== Known Issues ==
+
+== Changelog ==
+
+= 1.0.0 =
+*    Init version
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+*    Just Init version
---
+++ zbPlayer.php
@@ -0,0 +1,82 @@
+<?php
+/*
+Plugin Name: zbPlayer
+Plugin URI: http://gilevich.com/portfolio/zbplayer
+Description: Converts mp3 files links to a small flash player and a link to download file mp3 file. Player by outdated plugin <a href="http://wpaudioplayer.com/">WordPress Audio Player</a>.
+Version: 1.0
+Author: Vladimir Gilevich
+Author URI: http://gilevich.com/
+****************************************************
+/*
+ *	zbPlayer Wordpress Plugin
+ *	(c) 20013 Vladimir Gilevich
+ *	Dual Licensed under the MIT and GPL licenses
+ *  See license.txt, included with this package for more
+ *
+ *	zbPlayer.php
+ *  Release 1.0, January 2013
+ */
+?>
+<?php
+
+const ZB_PLAYER_VERSION = "1.0";
+const ZB_DEFAULT_WIDTH = "300";
+
+// Hook to add scripts
+add_action('admin_menu','zbp_add_pages');
+add_filter('the_content', 'zbp_content');
+add_action("plugins_loaded", "zbp_init");
+
+function zbp_init() {
+	if (get_option('zbp_width') <= 0) {
+		update_option('zbp_width',ZB_DEFAULT_WIDTH);
+	}
+}
+
+// Replace mp3 links in content with player 
+function zbp_content($content)
+{
+  // Replace mp3 links (don't do this in feeds and excerpts)
+  if ( !is_feed() ) {
+    $pattern = "/<a ([^=]+=['\"][^\"']+['\"] )*href=['\"](([^\"']+\.mp3))['\"]( [^=]+=['\"][^\"']+['\"])*>([^<]+)<\/a>/i";
+    $content = preg_replace_callback( $pattern, "zbp_insert_player", $content );
+  }
+  return $content;
+}
+
+// Main code - insert player into content
+function zbp_insert_player($matches)
+{
+  $link = preg_split("/[\|]/", $matches[3]);
+  $link = $link[0];
+  $name = $matches[5];
+	$download = get_option('zbp_download') == 'true' ? '<span> [<a href="'.$link.'" class="zbPlayer-download">Download</a>] </span>' : '';
+	$autostart = get_option('zbp_autostart') == 'true' ? 'yes' : 'no';
+	$width = get_option('zbp_width') > 0 ? intval(get_option('zbp_width')) : ZB_DEFAULT_WIDTH;
+  $ret = '<div class="zbPlayer"><a href="'.$link.'" class="zbPlayer">'.$name.'</a>' . $download
+   . '<embed width="'.$width.'" height="26" wmode="transparent" menu="false" quality="high"'
+		. ' flashvars="playerID=zbPlayer&amp;titles='.$name.'&amp;soundFile='.urlencode($link)
+		. '&amp;autostart='.$autostart.'" type="application/x-shockwave-flash" class="player" src="/wp-content/plugins/zbplayer/data/player.swf" id="zbPlayer"/></div>';
+  return $ret;
+}
+
+
+// See if we need to install/update
+if (get_option('zbp_version') != ZB_PLAYER_VERSION) zbp_setup(ZB_PLAYER_VERSION);
+
+// Add the script
+function zbp_add_pages() {
+	// Add a new submenu under options
+	add_options_page('zbPlayer','zbPlayer',6,'zbplayer','zbp_manage_page');
+}
+
+// Management Page
+function zbp_manage_page() {
+	include_once('zbPlayer.admin.php');
+}
+
+
+// Setup Function
+function zbp_setup($ZB_PLAYER_VERSION) {
+	update_option('zbp_version',$ZB_PLAYER_VERSION);
+}
