1ca33fbdd029c7cfb11d0fde8508de4c57938600
zubaka <zubaka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-22 15:13:21 +0000

    Tag 1.4: Path fixes
    git-svn-id: https://plugins.svn.wordpress.org/zbplayer/tags/1.4@671770 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,64 @@
+===zbPlayer ===
+Contributors: zubaka
+Donate link: http://gilevich.com/
+Tags: mp3, flash player, audio, easy control, noJS, small mp3 player, embed, media player, music player, mp3 player, cyrillic mp3 player
+Requires at least: 3.5
+Tested up to: 3.5
+Stable tag: 1.4
+License: Dual Licensed under the MIT and GPLv2 or later
+
+zbPlayer is a small and very easy plugin. It does one thing: capture mp3 links and insert a small flash player instead.
+
+== Description ==
+
+zbPlayer is a very easy audio plugin - you can select some options like: include Download link or no, enable autoplay or no and setup width of player. Other things will be done by zbPlayer plugun automatically. One nice feature - player support cyrillic filenames without problem.
+
+== Installation ==
+
+The most basic installation is a simple two step:
+
+1. Upload the `zbplayer` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+That's it, you're done.
+
+== Frequently Asked Questions ==
+
+= Do I need to do anything special in my posts? =
+
+No, zbPlayer automatically converts any link to an mp3 file into player. So, if you put in &lt;a href="audio_file.mp3"&gt;A Link&lt;/a&gt; zbPlayer will automatically add a flash player on page.
+
+= Does player support cyrillic names for mp3 files? =
+
+Yes, here no problems with cyrillic filenames.
+
+== Screenshots ==
+
+1.  The minimized player.
+2.  The expanded player.
+
+
+== Known Issues ==
+
+== Changelog ==
+
+= 1.4 =
+*    Fixed problem with player path if wordpress works not from default directory
+*    Fixed pathinfo function - now it works with UTF-8 encoded file names too
+
+= 1.3 =
+*    Allow to play files with cyrillic filenames.
+
+= 1.2 =
+*    Increased player width. Now player looks better.
+
+= 1.1 =
+*    Fix constants namings
+
+= 1.0 =
+*    Init version
+
+== Upgrade Notice ==
+
+= 1.0 =
+*    Just Init version
---
+++ zbPlayer.php
@@ -0,0 +1,109 @@
+<?php
+/*
+Plugin Name: zbPlayer
+Plugin URI: http://gilevich.com/portfolio/zbplayer
+Description: Converts mp3 files links to a small flash player and a link to download file mp3 file. Player by outdated plugin <a href="http://wpaudioplayer.com/">WordPress Audio Player</a>.
+Version: 1.4
+Author: Vladimir Gilevich
+Author URI: http://gilevich.com/
+****************************************************
+/*
+ *	zbPlayer Wordpress Plugin
+ *	(c) 20013 Vladimir Gilevich
+ *	Dual Licensed under the MIT and GPL licenses
+ *  See license.txt, included with this package for more
+ *
+ *	zbPlayer.php
+ *  Release 1.4, February 2013
+ */
+
+define('ZBPLAYER_VERSION', "1.4");
+define('ZBPLAYER_DEFAULT_WIDTH', "500");
+
+// Hook to add scripts
+add_action('admin_menu','zbp_add_pages');
+add_filter('the_content', 'zbp_content');
+add_action("plugins_loaded", "zbp_init");
+
+function zbp_init() {
+	if (get_option('zbp_width') <= 0) {
+		update_option('zbp_width',ZBPLAYER_DEFAULT_WIDTH);
+	}
+}
+
+// Replace mp3 links in content with player 
+function zbp_content($content)
+{
+  // Replace mp3 links (don't do this in feeds and excerpts)
+  if ( !is_feed() ) {
+    $pattern = "/<a ([^=]+=['\"][^\"']+['\"] )*href=['\"](([^\"']+\.mp3))['\"]( [^=]+=['\"][^\"']+['\"])*>([^<]+)<\/a>/i";
+    $content = preg_replace_callback( $pattern, "zbp_insert_player", $content );
+  }
+  return $content;
+}
+
+// Main code - insert player into content
+function zbp_insert_player($matches)
+{
+  $link = preg_split("/[\|]/", $matches[3]);
+  $link = $link[0];
+  $name = $matches[5];
+	$download = get_option('zbp_download') == 'true' ? '<span> [<a href="'.$link.'" class="zbPlayer-download">Download</a>] </span>' : '';
+	$autostart = get_option('zbp_autostart') == 'true' ? 'yes' : 'no';
+	$width = get_option('zbp_width') > 0 ? intval(get_option('zbp_width')) : ZBPLAYER_DEFAULT_WIDTH;
+  $ret = '<div class="zbPlayer">' . $name . $download . '<br/>'
+   . '<embed width="'.$width.'" height="26" wmode="transparent" menu="false" quality="high"'
+		. ' flashvars="playerID=zbPlayer&amp;titles='.$name.'&amp;soundFile='.zbp_urlencode($link)
+		. '&amp;autostart='.$autostart.'" type="application/x-shockwave-flash" class="player" src="'.plugin_dir_url(__FILE__).'data/player.swf" id="zbPlayer"/></div>';
+  return $ret;
+}
+
+// own ulrencode method - need to convert to utf8 filename if it is not in utf8
+function zbp_urlencode($link)
+{
+	$url = parse_url($link);
+	$file = zbp_mb_pathinfo($url['path']);
+
+	// prepare filename and encode if need
+	$filename = function_exists('mb_detect_encoding') && mb_detect_encoding($file['basename']) != "UTF-8" ? utf8_encode($file['basename']) : $file['basename']; 
+
+	$link = $url['scheme'] . '://' . $url['host'] . $file['dirname'] . '/' . zbp_flash_entities(urlencode($filename));
+	return $link;
+}
+
+// pathinfo with UTF-8 encoded file names too. Special thanks Pietro Baricco
+function zbp_mb_pathinfo($filepath)
+{
+	preg_match('%^(.*?)[\\\\/]*(([^/\\\\]*?)(\.([^\.\\\\/]+?)|))[\\\\/\.]*$%im',$filepath,$m);
+	if($m[1]) $ret['dirname']=$m[1];
+	if($m[2]) $ret['basename']=$m[2];
+	if($m[5]) $ret['extension']=$m[5];
+	if($m[3]) $ret['filename']=$m[3];
+	return $ret;
+}
+
+// replace special symbols to do not destoy flash vars
+function zbp_flash_entities($string)
+{ 
+	return str_replace(array("%", "&","'"),array("%25","%26","%27"),$string); 
+} 
+
+// See if we need to install/update
+if (get_option('zbp_version') != ZBPLAYER_VERSION) zbp_setup(ZBPLAYER_VERSION);
+
+// Add the script
+function zbp_add_pages() {
+	// Add a new submenu under options
+	add_options_page('zbPlayer','zbPlayer',6,'zbplayer','zbp_manage_page');
+}
+
+// Management Page
+function zbp_manage_page() {
+	include_once('zbPlayer.admin.php');
+}
+
+
+// Setup Function
+function zbp_setup($ZBPLAYER_VERSION) {
+	update_option('zbp_version',$ZBPLAYER_VERSION);
+}
