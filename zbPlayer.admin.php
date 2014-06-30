1eb1439b71a749bd5d7b0851fef1e585891ae951
zubaka <zubaka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-30 20:26:13 +0000

    Small codestyle changes
    git-svn-id: https://plugins.svn.wordpress.org/zbplayer/trunk@941214 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- zbPlayer.admin.php
+++ zbPlayer.admin.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  *	zbPlayer Wordpress Plugin
  *	(c) 2013-2014 Vladimir Gilevich
  *	Dual Licensed under the MIT and GPL licenses
@@ -8,8 +8,7 @@
  *	zbPlayer.admin.php
  *  Release 2.1.4 April 2014
  */
-?>
-<?php // connect wordpress color picker
+// connect wordpress color picker
 WP_Enqueue_Script('farbtastic');
 WP_Enqueue_Style('farbtastic');
 WP_Enqueue_Script('zbplayer-options', get_bloginfo('wpurl').'/'.Str_Replace("\\", '/', SubStr(RealPath(DirName(__FILE__)), Strlen(ABSPATH))) . '/js/zbPlayerColors.js');
@@ -28,89 +27,93 @@ WP_Enqueue_Script('zbplayer-options', get_bloginfo('wpurl').'/'.Str_Replace("\\"
 <?php
 // Business Logic
 if (isset($_POST['action'])) {
-	if (!isset($_POST['_wpnonce'])) die("There was a problem authenticating. Please log out and log back in");
-	if (!check_admin_referer('zbp-update_options')) die("There was a problem authenticating. Please log out and log back in");
-	if ($_POST['action'] == 'update') {
-		if (isset($_POST['zbp_width'])) {
-			update_option('zbp_width',intval($_POST['zbp_width']));
-		} else {
-			update_option('zbp_width',ZBPLAYER_DEFAULT_WIDTH);
-		}
-		if (isset($_POST['zbp_autostart'])) {
-			update_option('zbp_autostart','true');
-		} else {
-			update_option('zbp_autostart','false');
-		}
-		if (isset($_POST['zbp_loop'])) {
-			update_option('zbp_loop','true');
-		} else {
-			update_option('zbp_loop','false');
-		}
-		if (isset($_POST['zbp_show_name'])) {
-			update_option('zbp_show_name','Y');
-		} else {
-			update_option('zbp_show_name','N');
-		}
-		if (isset($_POST['zbp_download'])) {
-			update_option('zbp_download','true');
-		} else {
-			update_option('zbp_download','false');
-		}
-		if (isset($_POST['zbp_id3'])) {
-			update_option('zbp_id3','true');
-		} else {
-			update_option('zbp_id3','false');
-		}
-		if (isset($_POST['zbp_animation'])) {
-			update_option('zbp_animation','true');
-		} else {
-			update_option('zbp_animation','false');
-		}
-		if (isset($_POST['zbp_collect_mp3'])) {
-			update_option('zbp_collect_mp3','true');
-		} else {
-			update_option('zbp_collect_mp3','false');
-		}
-		if (isset($_POST['zbp_collect_field'])) {
-			update_option('zbp_collect_field',$_POST['zbp_collect_field']);
-		} else {
-			update_option('zbp_collect_field',ZBPLAYER_DEFAULT_COLLECT_FIELD);
-		}
-		if (isset($_POST['zbp_initialvolume'])) {
-			update_option('zbp_initialvolume',intval($_POST['zbp_initialvolume']));
-		} else {
-			update_option('zbp_initialvolume',ZBPLAYER_DEFAULT_INITIALVOLUME);
-		}
-		if (isset($_POST['zbp_show_share'])) {
-			update_option('zbp_show_share','true');
+    if (!isset($_POST['_wpnonce'])) {
+        die("There was a problem authenticating. Please log out and log back in");   
+    }
+    if (!check_admin_referer('zbp-update_options')) {
+        die("There was a problem authenticating. Please log out and log back in");   
+    }
+    if ($_POST['action'] == 'update') {
+        if (isset($_POST['zbp_width'])) {
+            update_option('zbp_width',intval($_POST['zbp_width']));
+        } else {
+            update_option('zbp_width',ZBPLAYER_DEFAULT_WIDTH);
+        }
+        if (isset($_POST['zbp_autostart'])) {
+            update_option('zbp_autostart','true');
+        } else {
+            update_option('zbp_autostart','false');
+        }
+        if (isset($_POST['zbp_loop'])) {
+            update_option('zbp_loop','true');
+        } else {
+            update_option('zbp_loop','false');
+        }
+        if (isset($_POST['zbp_show_name'])) {
+            update_option('zbp_show_name','Y');
+        } else {
+            update_option('zbp_show_name','N');
+        }
+        if (isset($_POST['zbp_download'])) {
+            update_option('zbp_download','true');
+        } else {
+            update_option('zbp_download','false');
+        }
+        if (isset($_POST['zbp_id3'])) {
+            update_option('zbp_id3','true');
+        } else {
+            update_option('zbp_id3','false');
+        }
+        if (isset($_POST['zbp_animation'])) {
+            update_option('zbp_animation','true');
+        } else {
+            update_option('zbp_animation','false');
+        }
+        if (isset($_POST['zbp_collect_mp3'])) {
+            update_option('zbp_collect_mp3','true');
+        } else {
+            update_option('zbp_collect_mp3','false');
+        }
+        if (isset($_POST['zbp_collect_field'])) {
+            update_option('zbp_collect_field',$_POST['zbp_collect_field']);
+        } else {
+            update_option('zbp_collect_field',ZBPLAYER_DEFAULT_COLLECT_FIELD);
+        }
+        if (isset($_POST['zbp_initialvolume'])) {
+            update_option('zbp_initialvolume',intval($_POST['zbp_initialvolume']));
+        } else {
+            update_option('zbp_initialvolume',ZBPLAYER_DEFAULT_INITIALVOLUME);
+        }
+        if (isset($_POST['zbp_show_share'])) {
+            update_option('zbp_show_share','true');
 
-			if (isset($_POST['zbp_share']) && $_POST['zbp_share'] == ZBPLAYER_SHARE_SMALL) {
-				update_option('zbp_share',ZBPLAYER_SHARE_SMALL);
-			} else {
-				update_option('zbp_share',ZBPLAYER_SHARE_INLINE);
-			}
-		} else {
-			update_option('zbp_show_share','false');
-		}
-		?><div class="updated"><p><strong>Options Updated</strong></p></div><?php
-	} else if ($_POST['action'] == 'updateColor') {
-		update_option('zbp_bg_color',$_POST['zbp_bg_color']);
-		update_option('zbp_bg_left_color',$_POST['zbp_bg_left_color']);
-		update_option('zbp_icon_left_color',$_POST['zbp_icon_left_color']);
-		update_option('zbp_voltrack_color',$_POST['zbp_voltrack_color']);
-		update_option('zbp_volslider_color',$_POST['zbp_volslider_color']);
-		update_option('zbp_bg_right_color',$_POST['zbp_bg_right_color']);
-		update_option('zbp_bg_right_hover_color',$_POST['zbp_bg_right_hover_color']);
-		update_option('zbp_icon_right_color',$_POST['zbp_icon_right_color']);
-		update_option('zbp_icon_right_hover_color',$_POST['zbp_icon_right_hover_color']);
-		update_option('zbp_loader_color',$_POST['zbp_loader_color']);
-		update_option('zbp_track_color',$_POST['zbp_track_color']);
-		update_option('zbp_tracker_color',$_POST['zbp_tracker_color']);
-		update_option('zbp_border_color',$_POST['zbp_border_color']);
-		update_option('zbp_skip_color',$_POST['zbp_skip_color']);
-		update_option('zbp_text_color',$_POST['zbp_text_color']);
-		?><div class="updated"><p><strong>Colors Updated</strong></p></div><?php
-	}
+            if (isset($_POST['zbp_share']) && $_POST['zbp_share'] == ZBPLAYER_SHARE_SMALL) {
+                update_option('zbp_share',ZBPLAYER_SHARE_SMALL);
+            } else {
+                update_option('zbp_share',ZBPLAYER_SHARE_INLINE);
+            }
+        } else {
+            update_option('zbp_show_share','false');
+        }
+        ?><div class="updated"><p><strong>Options Updated</strong></p></div><?php
+    } else if ($_POST['action'] == 'updateColor') {
+        update_option('zbp_bg_color',$_POST['zbp_bg_color']);
+        update_option('zbp_bg_left_color',$_POST['zbp_bg_left_color']);
+        update_option('zbp_icon_left_color',$_POST['zbp_icon_left_color']);
+        update_option('zbp_voltrack_color',$_POST['zbp_voltrack_color']);
+        update_option('zbp_volslider_color',$_POST['zbp_volslider_color']);
+        update_option('zbp_bg_right_color',$_POST['zbp_bg_right_color']);
+        update_option('zbp_bg_right_hover_color',$_POST['zbp_bg_right_hover_color']);
+        update_option('zbp_icon_right_color',$_POST['zbp_icon_right_color']);
+        update_option('zbp_icon_right_hover_color',$_POST['zbp_icon_right_hover_color']);
+        update_option('zbp_loader_color',$_POST['zbp_loader_color']);
+        update_option('zbp_track_color',$_POST['zbp_track_color']);
+        update_option('zbp_tracker_color',$_POST['zbp_tracker_color']);
+        update_option('zbp_border_color',$_POST['zbp_border_color']);
+        update_option('zbp_skip_color',$_POST['zbp_skip_color']);
+        update_option('zbp_text_color',$_POST['zbp_text_color']);
+        ?><div class="updated"><p><strong>Colors Updated</strong></p></div><?php
+    }
 }
 
 
@@ -258,82 +261,83 @@ $imgPath = plugin_dir_url(__FILE__) . '/images/';
 <form id="zbp_color" name="zbp_color" action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
 <table class="form-table">
 <?php 
-	 foreach(array(
-						 array( 'title' => __('Background', 'zbplayer'),
-										'name' => 'zbp_bg_color',
-										'default' => ZBPLAYER_COLOR_BG,
-										'desc' => __('Background color inside the player. The environment of the player is always transparent.', 'zbplayer') ),
-						 
-						 array( 'title' => __('Background left', 'zbplayer'),
-										'name' => 'zbp_bg_left_color',
-										'default' => ZBPLAYER_COLOR_LEFTBG,
-										'desc' => __('Background color of the speaker icon background/volume control.', 'zbplayer') ),
+foreach(
+    array(
+        array( 'title' => __('Background', 'zbplayer'),
+               'name' => 'zbp_bg_color',
+               'default' => ZBPLAYER_COLOR_BG,
+               'desc' => __('Background color inside the player. The environment of the player is always transparent.', 'zbplayer') ),
 
-						 array( 'title' => __('Icon left', 'zbplayer'),
-										'name' => 'zbp_icon_left_color',
-										'default' => ZBPLAYER_COLOR_LEFTICON,
-										'desc' => __('Color of the speaker icon on the left hand.', 'zbplayer') ),
+        array( 'title' => __('Background left', 'zbplayer'),
+               'name' => 'zbp_bg_left_color',
+               'default' => ZBPLAYER_COLOR_LEFTBG,
+               'desc' => __('Background color of the speaker icon background/volume control.', 'zbplayer') ),
 
-						 array( 'title' => __('Volume track', 'zbplayer'),
-										'name' => 'zbp_voltrack_color',
-										'default' => ZBPLAYER_COLOR_VOLTRACK,
-										'desc' => __('Color of the volume track on the left hand.', 'zbplayer') ),
+        array( 'title' => __('Icon left', 'zbplayer'),
+               'name' => 'zbp_icon_left_color',
+               'default' => ZBPLAYER_COLOR_LEFTICON,
+               'desc' => __('Color of the speaker icon on the left hand.', 'zbplayer') ),
 
-						 array( 'title' => __('Volume slider', 'zbplayer'),
-										'name' => 'zbp_volslider_color',
-										'default' => ZBPLAYER_COLOR_VOLSLIDER,
-										'desc' => __('Color of the volume slider on the left hand.', 'zbplayer') ),
+        array( 'title' => __('Volume track', 'zbplayer'),
+               'name' => 'zbp_voltrack_color',
+               'default' => ZBPLAYER_COLOR_VOLTRACK,
+               'desc' => __('Color of the volume track on the left hand.', 'zbplayer') ),
 
-						 array( 'title' => __('Background right', 'zbplayer'),
-										'name' => 'zbp_bg_right_color',
-										'default' => ZBPLAYER_COLOR_RIGHTBG,
-										'desc' => __('Background color of the play/pause button.', 'zbplayer') ),
+        array( 'title' => __('Volume slider', 'zbplayer'),
+               'name' => 'zbp_volslider_color',
+               'default' => ZBPLAYER_COLOR_VOLSLIDER,
+               'desc' => __('Color of the volume slider on the left hand.', 'zbplayer') ),
 
-						 array( 'title' => __('Background right (hover)', 'zbplayer'),
-										'name' => 'zbp_bg_right_hover_color',
-										'default' => ZBPLAYER_COLOR_RIGHTBGHOVER,
-										'desc' => __('Color of the right background while hovering it with the mouse.', 'zbplayer') ),
+        array( 'title' => __('Background right', 'zbplayer'),
+               'name' => 'zbp_bg_right_color',
+               'default' => ZBPLAYER_COLOR_RIGHTBG,
+               'desc' => __('Background color of the play/pause button.', 'zbplayer') ),
 
-						 array( 'title' => __('Icon right', 'zbplayer'),
-										'name' => 'zbp_icon_right_color',
-										'default' => ZBPLAYER_COLOR_RIGHTICON,
-										'desc' => __('Color of the play/pause icon.', 'zbplayer') ),
+        array( 'title' => __('Background right (hover)', 'zbplayer'),
+               'name' => 'zbp_bg_right_hover_color',
+               'default' => ZBPLAYER_COLOR_RIGHTBGHOVER,
+               'desc' => __('Color of the right background while hovering it with the mouse.', 'zbplayer') ),
 
-						 array( 'title' => __('Icon right (hover)', 'zbplayer'),
-										'name' => 'zbp_icon_right_hover_color',
-										'default' => ZBPLAYER_COLOR_RIGHTICONHOVER,
-										'desc' => __('Color of the right icon while hovering it with the mouse.', 'zbplayer') ),
+        array( 'title' => __('Icon right', 'zbplayer'),
+               'name' => 'zbp_icon_right_color',
+               'default' => ZBPLAYER_COLOR_RIGHTICON,
+               'desc' => __('Color of the play/pause icon.', 'zbplayer') ),
 
-						 array( 'title' => __('Loading bar', 'zbplayer'),
-										'name' => 'zbp_loader_color',
-										'default' => ZBPLAYER_COLOR_LOADER,
-										'desc' => __('Color of the loading bar.', 'zbplayer') ),
+        array( 'title' => __('Icon right (hover)', 'zbplayer'),
+               'name' => 'zbp_icon_right_hover_color',
+               'default' => ZBPLAYER_COLOR_RIGHTICONHOVER,
+               'desc' => __('Color of the right icon while hovering it with the mouse.', 'zbplayer') ),
 
-						 array( 'title' => __('Track bar', 'zbplayer'),
-										'name' => 'zbp_track_color',
-										'default' => ZBPLAYER_COLOR_TRACK,
-										'desc' => __('Color of the track bar.', 'zbplayer') ),
+        array( 'title' => __('Loading bar', 'zbplayer'),
+               'name' => 'zbp_loader_color',
+               'default' => ZBPLAYER_COLOR_LOADER,
+               'desc' => __('Color of the loading bar.', 'zbplayer') ),
 
-						 array( 'title' => __('Tracker', 'zbplayer'),
-										'name' => 'zbp_tracker_color',
-										'default' => ZBPLAYER_COLOR_TRACKER,
-										'desc' => __('Color of the progress track (tracker).', 'zbplayer') ),
+        array( 'title' => __('Track bar', 'zbplayer'),
+               'name' => 'zbp_track_color',
+               'default' => ZBPLAYER_COLOR_TRACK,
+               'desc' => __('Color of the track bar.', 'zbplayer') ),
 
-						 array( 'title' => __('Track bar border', 'zbplayer'),
-										'name' => 'zbp_border_color',
-										'default' => ZBPLAYER_COLOR_BORDER,
-										'desc' => __('Color of the border surrounding the track bar.', 'zbplayer') ),
+        array( 'title' => __('Tracker', 'zbplayer'),
+               'name' => 'zbp_tracker_color',
+               'default' => ZBPLAYER_COLOR_TRACKER,
+               'desc' => __('Color of the progress track (tracker).', 'zbplayer') ),
 
-						 array( 'title' => __('Skip', 'zbplayer'),
-										'name' => 'zbp_skip_color',
-										'default' => ZBPLAYER_COLOR_SKIP,
-										'desc' => __('Previous/Next skip buttons.', 'zbplayer') ),
+        array( 'title' => __('Track bar border', 'zbplayer'),
+               'name' => 'zbp_border_color',
+               'default' => ZBPLAYER_COLOR_BORDER,
+               'desc' => __('Color of the border surrounding the track bar.', 'zbplayer') ),
 
-						 array( 'title' => __('Text', 'zbplayer'),
-										'name' => 'zbp_text_color',
-										'default' => ZBPLAYER_COLOR_TEXT,
-										'desc' => __('Color of the text in the middle area.', 'zbplayer') )
-					 ) as $color) : ?>
+        array( 'title' => __('Skip', 'zbplayer'),
+               'name' => 'zbp_skip_color',
+               'default' => ZBPLAYER_COLOR_SKIP,
+               'desc' => __('Previous/Next skip buttons.', 'zbplayer') ),
+
+        array( 'title' => __('Text', 'zbplayer'),
+               'name' => 'zbp_text_color',
+               'default' => ZBPLAYER_COLOR_TEXT,
+               'desc' => __('Color of the text in the middle area.', 'zbplayer') )
+    ) as $color) : ?>
 <tr valign="top">
   <th scope="row"><?php echo $color['title'] ?></th>
   <td>
